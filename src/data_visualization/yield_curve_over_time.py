1df34f6ab0b3f85779fc9994880bfabbefb5fcc3
alperyildirim <yildirimalper@outlook.de>
2024-04-21 15:50:50 +0300

    Add Japanese yield plots and BoC MP scraper

--- src/data_visualization/yield_curve_over_time.py
+++ src/data_visualization/yield_curve_over_time.py
@@ -10,19 +10,27 @@ plt.style.use('science')
 PROJECT_DIR = Path().resolve()
 
 # Load data
-data = pd.read_csv(PROJECT_DIR / 'processed_data' / 'yield_data' / 'swiss_spot_yields.csv')
+data = pd.read_csv(PROJECT_DIR / 'processed_data' / 'yield_data' / 'uk_spot_yields.csv')
 
 # Convert Date to datetime and set as index
 data.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
 #data.drop(columns={'Unnamed: 0'}, inplace=True) # for SNB
-data['Date'] = pd.to_datetime(data['Date']) # , format='%d/%m/%Y' for Ecb
+data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y') # for Ecb
 data.set_index('Date', inplace=True)
+data = data.iloc[:, ::-1]
+data.columns = data.columns.str.replace('yr', '', regex=True)
 
 # data = data.replace(r".*na.*", np.nan, regex=True) #for Canada
 # data = data.replace(' ', np.nan)
 # data = data.dropna() # again for Canada
-# data = data.dropna(thresh=len(data) - 1000, axis=1) #for UK
+# data = data.dropna(thresh=len(data) - 2200, axis=1) #for UK
 # data = data.dropna() #for UK
+# for Japan
+# data = data.drop(["15", "25", "30", "40"], axis=1)
+# data.columns = data.columns.str.replace('yr', '', regex=True)
+# data = data.apply(pd.to_numeric, errors='coerce')
+# data.index = pd.to_datetime(data.index, format='%d/%m/%Y')
+
 
 # Create high resolution 3D plot
 fig = plt.figure(figsize=(10, 6), dpi=300)
@@ -30,10 +38,12 @@ ax = fig.add_subplot(111, projection='3d')
 
 # Convert dates to number of days since first date
 X_numeric = (data.index - data.index[0]).days
-X_years = data.index.year
+#X_years = data.index.year
+X_years = data.index.year.dropna().astype(int)
+maturities = pd.to_numeric(data.columns)
 
 # Create 2D arrays for X, Y, and Z
-X, Y = np.meshgrid(X_numeric, np.arange(len(data.columns)))
+X, Y = np.meshgrid(X_numeric, maturities)
 Z = data.T.values  # Transpose DataFrame before extracting values
 
 # Plot surface
@@ -48,6 +58,6 @@ ax.set_ylabel('Maturity')
 ax.set_zlabel('Yield (\%)')
 
 # Save figure
-plt.savefig('figures/yield_curves/snb_yield_curve.png', format='png', dpi=300)
+plt.savefig('figures/yield_curves/boe_yield_curve.png', format='png', dpi=300)
 
 plt.show()
---
+++ src/fetch_data/fetch_boc_mp_decision_dates.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import csv
+import pandas as pd
+import datetime
+from pathlib import Path
+
+driver = webdriver.Chrome() 
+driver.get("https://www.moneycontrol.com/economic-calendar/canada-boc-monetary-policy-report/3069585 ")
+# /html/body/section/div/div/div[2]/div[5]/table/tbody/tr[4]/td[1]/span
+
+# Open the CSV file for writing
+with open('processed_data/monetary_policy_dates/boc_mpd_raw.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    # Repeat the following steps for each page
+    for _ in range(60):  # adjust the range depending on the number of pages
+        time.sleep(10)  # wait for the page to load
+
+        # Scroll down to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(4)  # wait for the page to load
+
+        # Click the "Show More" button
+        show_more_button = driver.find_elements(By.XPATH, '/html/body/section/div/div/div[2]/div[5]/div[2]/div/a')[0]
+        show_more_button.click()
+
+        # Extract dates
+        date_elements = driver.find_elements(By.XPATH, '/html/body/section/div/div/div[2]/div[5]/table/tbody/tr/td[1]/span')
+
+        # Write dates to the CSV file
+        for date_element in date_elements:
+            writer.writerow([date_element.text])
+
+# Close the driver
+driver.quit()
+
+# ===========================
+
+PROJECT_DIR = Path().resolve()
+
+data = pd.read_csv('processed_data/monetary_policy_dates/boc_mpd_raw.csv', header=None)
+
+# Drop duplicates
+data2 = data.drop_duplicates()
+data2 = data2.reset_index(drop=True)
+data2[0] = pd.to_datetime(data2[0])
+data2.to_csv(PROJECT_DIR / "processed_data" / "monetary_policy_dates" / "boc_mpd.csv", index=False) 
